86978355f5d4b1e54ecda677b95779d165e4c9d9
Muhammed_amil <amilasnils008@gmail.com>
2025-07-28 23:51:54 +0530

    updated the training_pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,6 +1,13 @@
 
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handling_missing_value_step import handle_missing_values_step
+from steps.feature_engineering_step import feature_engineering_step
+from steps.outliers_detection_step import outlier_detection_step
+from steps.data_splitter_step import data_splitter_step
+from steps.model_building_step import model_building_step
+
+
+
 
 from zenml import Model,pipeline, step
 
@@ -14,12 +21,22 @@ from zenml import Model,pipeline, step
 def ml_pipeline():
     # end-to-end ml pipeline
     raw_data = data_ingestion_step(file_path =
-                                   r"C:\Users\amil\OneDrive\Documents\end-to-end-house_prediction\data\archive.zip")
+                                r"C:\Users\amil\OneDrive\Documents\end-to-end-house_prediction\data\archive.zip")
     
     #next step is handling the missing values 
-    
     filled_data = handle_missing_values_step(raw_data)
     
+    engineered_data = feature_engineering_step(
+        filled_data, strategy="log", features=["Gr Liv Area", "SalePrice"]
+    )
+    
+    clean_data = outlier_detection_step(engineered_data, column_name="SalePrice")
+    
+    X_train, X_test, y_train, y_test = data_splitter_step(clean_data, target_column="SalePrice")
+    
+    # Model Building Step
+    model = model_building_step(X_train=X_train, y_train=y_train)
+
 
 
 
